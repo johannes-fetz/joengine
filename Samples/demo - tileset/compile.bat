8d6fb08b552b64d8811577333ef3870a07abd157
Johannes <johannesfetz@gmail.com>
2020-04-19 18:10:16 +0200

    fix compile.bat

--- Samples/demo - tileset/compile.bat
+++ Samples/demo - tileset/compile.bat
@@ -1,5 +1,6 @@
 @ECHO Off
 SET COMPILER_DIR=..\..\Compiler
 SET PATH=%COMPILER_DIR%\SH_COFF\Other Utilities;%COMPILER_DIR%\SH_COFF\sh-coff\bin;%COMPILER_DIR%\TOOLS;%PATH%
-ECHO Calling make with -j%NUMBER_OF_PROCESSORS%
-make -j%NUMBER_OF_PROCESSORS% re
+REM ECHO Calling make with -j%NUMBER_OF_PROCESSORS%
+REM make -j%NUMBER_OF_PROCESSORS% re
+make re
