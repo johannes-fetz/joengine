cbb706ca4ec2adb72b622cdc7dddcf37ba356d59
johannes-fetz <johannesfetz@gmail.com>
2020-06-01 16:56:36 +0200

    New GCC ELF SH2 9.3.0 (windows only)

--- Samples/demo1/clean.sh
+++ Samples/demo1/clean.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 rm -f ./cd/0.bin
 rm -f *.o
-rm -f ../jo_engine/*.o
-rm -f ./sl_coff.bin
-rm -f ./sl_coff.coff
-rm -f ./sl_coff.map
-rm -f ./sl_coff.iso
+rm -f ../../jo_engine/*.o
+rm -f ./*.bin
+rm -f ./*.coff
+rm -f ./*.elf
+rm -f ./*.map
+rm -f ./*.iso
+rm -f ./*.cue
 echo "Done."
 exit 0
--- Samples/demo1/run_with_yabause.sh
+++ Samples/demo1/run_with_yabause.sh
@@ -2,9 +2,9 @@
 command -v yabause >/dev/null 2>&1 || { echo "yabause is not installed.\
  Aborting." >&2; exit 1; }
 
-if [ -f sl_coff.iso ];
+if [ -f game.iso ];
 then
-   yabause -a -i sl_coff.cue
+   yabause -a -i game.cue
 else
    echo "Please compile first !" >&2
 fi
