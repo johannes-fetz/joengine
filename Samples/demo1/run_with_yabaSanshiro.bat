04ba190864fc62ab9f8db70bbed148bec26dee2d
johannes-fetz <johannesfetz@gmail.com>
2020-06-01 18:40:24 +0200

    Windows build fix

--- Samples/demo1/run_with_yabaSanshiro.bat
+++ Samples/demo1/run_with_yabaSanshiro.bat
@@ -2,6 +2,7 @@
 SET EMULATOR_DIR=..\..\Emulators
 
 if exist game.iso (
+echo Please wait, it's very slow, but I don't know why...
 "%EMULATOR_DIR%\YabaSanshiro\yabasanshiro.exe" -a -i game.iso
 ) else (
 echo Please compile first !
